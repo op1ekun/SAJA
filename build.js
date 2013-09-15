c7c0fa4979113c5c2878b6c4a354fa8ff533bbe9
op1ekun <woociek@gmail.com>
2013-09-15 11:46:35 +0100

    r.js optimizer in use

---
+++ build.js
@@ -0,0 +1,16 @@
+{
+	// appDir   : 'app/',
+	baseUrl  : 'app/',
+	// dir      : 'build/',
+	paths	 : {
+		pubsub	: '../lib/components/pubsub/pubsub',
+		jquery	: '../lib/components/jquery/jquery'
+	},
+	name	: 'bootstrap',
+	out		: 'app-min.js'
+	// modules : [
+	// 	{
+	// 		name : 'bootstrap'
+	// 	}
+	// ]
+}
